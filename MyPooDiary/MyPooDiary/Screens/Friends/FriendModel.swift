a542a3e45826db1f7d7a543ed4a17508397d8d6b
kpk0616 <kpk0616@naver.com>
2022-11-20 01:39:47 +0900

    [Add] #4 - FriendModel 추가

---
+++ MyPooDiary/MyPooDiary/Screens/Friends/FriendModel.swift
@@ -0,0 +1,13 @@
+//
+//  FriendModel.swift
+//  MyPooDiary
+//
+//  Created by 박의서 on 2022/11/20.
+//
+
+import Foundation
+
+struct FriendModel {
+    let friendImage: String
+    let name: String
+}
--- MyPooDiary/MyPooDiary/Screens/Friends/FriendsVIewController.swift
+++ MyPooDiary/MyPooDiary/Screens/Friends/FriendsVIewController.swift
@@ -17,12 +17,15 @@ final class FriendsViewController: UIViewController {
     // MARK: - Properties
     
     // MARK: - UI
+    private let topBar =  UIView()
+    private lazy var backButton = makeButton("")
     
     // MARK: - View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setUI()
+        setLayout()
     }
     
     // MARK: - Functions
@@ -32,7 +35,40 @@ final class FriendsViewController: UIViewController {
 }
 
 // MARK: - Extensions
-
+extension FriendsViewController {
+    
+    //MARK: - Custom Method
+    
+    private func setUI(){
+        view.backgroundColor = .white
+    }
+    
+    private func setLayout() {
+        view.addSubviews(topBar)
+        
+        // MARK: - TopBar View Constraints
+        topBar.backgroundColor = .systemGray2
+        topBar.addSubviews(backButton)
+        topBar.snp.makeConstraints {
+            $0.top.leading.trailing.equalTo(view.safeAreaLayoutGuide)
+            $0.height.equalTo(66)
+        }
+        backButton.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(16)
+            $0.leading.equalToSuperview().inset(27)
+            $0.width.equalTo(25)
+            $0.height.equalTo(34)
+        }
+    }
+    
+    private func makeButton(_ imageString: String)-> UIButton{
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(named: imageString), for: .normal)
+        button.contentMode = .scaleAspectFit
+        button.backgroundColor = .systemGray
+        return button
+    }
+}
 
 // MARK: - Network
 
