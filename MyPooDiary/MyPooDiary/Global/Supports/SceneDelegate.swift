c9c563fdcc3a875afa576d2f91a94a835e29abba
kpk0616 <kpk0616@naver.com>
2022-11-20 05:34:51 +0900

    [Chore] #4 - SceneDelegate 루트뷰컨 MainViewController 로

--- MyPooDiary/MyPooDiary/Global/Supports/SceneDelegate.swift
+++ MyPooDiary/MyPooDiary/Global/Supports/SceneDelegate.swift
@@ -17,7 +17,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = windowScene
-        window?.rootViewController = FriendListViewController()
+        window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
 
