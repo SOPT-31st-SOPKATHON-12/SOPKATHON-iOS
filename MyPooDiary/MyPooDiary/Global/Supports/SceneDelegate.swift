9220aeb59d236c64b64e4fc79b729491b1a57326
kpk0616 <kpk0616@naver.com>
2022-11-20 07:35:51 +0900

    test

--- MyPooDiary/MyPooDiary/Global/Supports/SceneDelegate.swift
+++ MyPooDiary/MyPooDiary/Global/Supports/SceneDelegate.swift
@@ -20,6 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         window?.rootViewController = MainViewController()
         window?.makeKeyAndVisible()
     }
+    
 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
