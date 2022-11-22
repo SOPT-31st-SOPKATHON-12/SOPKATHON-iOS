7c30d74894b6b8981b49043bf7c8e774d3343611
devxsby <devxsby@gmail.com>
2022-11-22 21:37:39 +0900

    [Add] #14- 이미지 에셋 추가 및 그래픽 이미지 리터럴 등록

--- MyPooDiary/MyPooDiary/Global/Literals/ImageLiterals.swift
+++ MyPooDiary/MyPooDiary/Global/Literals/ImageLiterals.swift
@@ -10,8 +10,44 @@ import UIKit
 enum ImageLiterals {
     
     struct Images {
-        static var sampleImg: UIImage { .load(named: "sampleImg") }
         
+        static var defaultImg: UIImage { .load(named: "img_defalt") }
+        
+        static var goodImg: UIImage { .load(named: "img_good") }
+        static var goodNobkImg: UIImage { .load(named: "img_good_nobk") }
+        
+        static var hardBlackNobkImg: UIImage { .load(named: "img_hard_black_nobk") }
+        static var hardBlackImg: UIImage { .load(named: "img_hard_black") }
+        static var hardBrownNobkImg: UIImage { .load(named: "img_hard_brown_nobk") }
+        static var hardBrownImg: UIImage { .load(named: "img_hard_brown") }
+        static var hardGreenNobkImg: UIImage { .load(named: "img_hard_green_nobk") }
+        static var hardGreenImg: UIImage { .load(named: "img_green_brown") }
+        static var hardRedNobkImg: UIImage { .load(named: "img_hard_red_nobk") }
+        static var hardRedImg: UIImage { .load(named: "img_hard_red") }
+        static var hardYellowNobkImg: UIImage { .load(named: "img_hard_yellow_nobk") }
+        static var hardYellowImg: UIImage { .load(named: "img_hard_yellow") }
+        
+        static var regularBlackNobkImg: UIImage { .load(named: "img_regular_black_nobk") }
+        static var regularBlackImg: UIImage { .load(named: "img_regular_black") }
+        static var regularBrownNobkImg: UIImage { .load(named: "img_regular_brown_nobk") }
+        static var regularBrownImg: UIImage { .load(named: "img_regular_brown") }
+        static var regularGreenNobkImg: UIImage { .load(named: "img_regular_green_nobk") }
+        static var regularGreenImg: UIImage { .load(named: "img_regular_brown") }
+        static var regularRedNobkImg: UIImage { .load(named: "img_regular_red_nobk") }
+        static var regularRedImg: UIImage { .load(named: "img_regular_red") }
+        static var regularYellowNobkImg: UIImage { .load(named: "img_regular_yellow_nobk") }
+        static var regularYellowImg: UIImage { .load(named: "img_regular_yellow") }
+        
+        static var softBlackNobkImg: UIImage { .load(named: "img_soft_black_nobk") }
+        static var softBlackImg: UIImage { .load(named: "img_soft_black") }
+        static var softBrownNobkImg: UIImage { .load(named: "img_soft_brown_nobk") }
+        static var softBrownImg: UIImage { .load(named: "img_soft_brown") }
+        static var softGreenNobkImg: UIImage { .load(named: "img_soft_green_nobk") }
+        static var softGreenImg: UIImage { .load(named: "img_soft_brown") }
+        static var softRedNobkImg: UIImage { .load(named: "img_soft_red_nobk") }
+        static var softRedImg: UIImage { .load(named: "img_soft_red") }
+        static var softYellowNobkImg: UIImage { .load(named: "img_soft_yellow_nobk") }
+        static var softYellowImg: UIImage { .load(named: "img_soft_yellow") }
     }
     
     struct Icons {
